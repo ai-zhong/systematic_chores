c991a2944d0fc19ddab76949f34cb2fce12aa42e
ai-zhong <aizhong.ds@gmail.com>
2022-04-09 15:05:54 -0700

    test commit

---
+++ kitchen/clean_after_cooking.py
@@ -0,0 +1,3 @@
+def clean_cooking_related_area():
+    # stove top, microwave, wall and cupboards near stove (especially if hot oil is used)
+    pass
---
+++ kitchen/dish_washing.py
@@ -0,0 +1,45 @@
+
+def collect_dish():
+    # dishes include: pot, dish, utensils
+    # collect everything to sink first for further processing
+    pass
+
+
+def remove_left_over_in_dish():
+    # liquid goes to sink
+    # solid waste goes to trash bin (compost)
+    pass
+
+
+def put_dishes_to_washing_machine():
+    # dirty debris may left on counter top, sink, floor
+    pass
+
+
+def put_dishwasher_detergent_to_machine():
+    # remember where are the detergent, and take note when stock is low.
+    pass
+
+
+def operate_dishwasher():
+    # eco mode
+    # full cycle mode
+    pass
+
+
+def clean_up_surfaces():
+    # clean anywhere that's being polluted.
+    pass
+
+
+def main():
+    collect_dish()
+    remove_left_over_in_dish()
+    put_dishes_to_washing_machine()
+    put_dishwasher_detergent_to_machine()
+    operate_dishwasher()
+    clean_up_surfaces()
+
+
+if __name__ == "__main__":
+    main()
